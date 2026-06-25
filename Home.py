ceb293d4a4a8cd75337963ad8d231d79e20d83bf
ririda <136903266+Ririda-tech@users.noreply.github.com>
2026-06-25 09:39:58 +0700

    Upload Dashboard Pegawai

---
+++ Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Pegawai Kampus XYZ")
+
+st.title("Dashboard Pegawai Kampus XYZ")
+
+st.write("""
+Selamat datang.
+
+Silakan buka menu di sidebar:
+
+1. Data Cleaning
+2. Dashboard Pegawai
+""")
